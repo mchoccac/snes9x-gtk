ef0bf15eb0079e679d1ea9c496aeca6cce320803
bearoso <bearoso@c0138a3e-f94e-11dd-a7be-4b3ef3b0700c>
2009-05-17 20:11:52 +0000

    Tag revision 71.

---
+++ tags/1.51.71/gtk/src/gtk_sound.h
@@ -0,0 +1,13 @@
+#ifndef __GTK_SOUND_H
+#define __GTK_SOUND_H
+
+void S9xPortSoundInit (void);
+void S9xPortSoundDeinit (void);
+void S9xSoundStart (void);
+void S9xSoundStop (void);
+
+extern int playback_rates[8];
+extern int buffer_sizes[8];
+extern double d_playback_rates[8];
+
+#endif /* __GTK_SOUND_H */

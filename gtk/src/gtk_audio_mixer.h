35f1b6f2fca4aeaa5aca062e638669a83fbd94d9
bearoso <bearoso@c0138a3e-f94e-11dd-a7be-4b3ef3b0700c>
2009-06-20 07:30:09 +0000

    Commit full source.

---
+++ gtk/src/gtk_audio_mixer.h
@@ -0,0 +1,31 @@
+#ifndef __GTK_AUDIO_MIXER_H
+#define __GTK_AUDIO_MIXER_H
+
+class GtkAudioMixer
+{
+    public:
+        GtkAudioMixer (int buffer_size);
+        ~GtkAudioMixer (void);
+        void start (void);
+        void stop (void);
+        bool write (unsigned char *output, int bytes);
+        bool is_ready (void);
+
+    private:
+        void mixer_thread (void);
+        int space_available (void);
+        int bytes_available (void);
+        unsigned char *internal_buffer;
+        int thread_die;
+        int start_byte;
+        int used;
+        int buffer_size;
+        GMutex *buffer_mutex;
+        GMutex *external_buffer_mutex;
+        GThread *thread;
+
+    friend gpointer gtk_audio_mixer_thread (gpointer data);
+};
+
+
+#endif /* __GTK_AUDIO_MIXER_H */

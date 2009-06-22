51a00375598ce3c8ea2dff2fd835970c7e5410d3
bearoso <bearoso@c0138a3e-f94e-11dd-a7be-4b3ef3b0700c>
2009-06-22 22:57:58 +0000

    Remove gtk_audio_mixer.

--- gtk/src/gtk_sound_driver_sdl.cpp
+++ gtk/src/gtk_sound_driver_sdl.cpp
@@ -95,16 +95,16 @@ S9xSDLSoundDriver::open_device (int mode, bool8 stereo, int buffer_size)
     audiospec->channels = so.stereo ? 2 : 1;
     audiospec->format = so.sixteen_bit ? AUDIO_S16SYS : AUDIO_U8;
     audiospec->samples = gui_config->sound_buffer_size * audiospec->freq / 1000;
-    /* Note: divide samples by 2 because SDL thinks it needs a whole buffer on
+    /* Note: divide samples by 16 because SDL thinks it needs a whole buffer on
      * every callback. :-P */
-    audiospec->samples = powerof2 (base2log (audiospec->samples)) >> 1;
+    audiospec->samples = powerof2 (base2log (audiospec->samples)) >> 4;
     audiospec->callback = sdl_audio_callback;
     audiospec->userdata = this;
 
     printf ("SDL sound driver initializing...\n");
     printf ("    --> (Frequency: %dhz, Latency: %dms)...",
             audiospec->freq,
-            audiospec->samples * 1000 / audiospec->freq);
+            (audiospec->samples * 1000 / audiospec->freq) << 4);
 
     if (SDL_OpenAudio (audiospec, NULL) < 0)
     {

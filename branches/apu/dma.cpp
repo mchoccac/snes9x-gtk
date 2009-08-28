9a9637979e68619a0686ba447ff6be1e4284ea87
bearoso <bearoso@c0138a3e-f94e-11dd-a7be-4b3ef3b0700c>
2009-08-28 18:42:12 +0000

    Remove extra APU execution lines. It needs only to sync on communication, and only the CPU can initiate this.

--- branches/apu/dma.cpp
+++ branches/apu/dma.cpp
@@ -184,7 +184,6 @@ static inline bool8 addCyclesInDMA (uint8 dma_channel)
 	// Add 8 cycles per byte, sync APU, and do HC related events.
 	// If HDMA was done in S9xDoHEventProcessing(), check if it used the same channel as DMA.
 	CPU.Cycles += SLOW_ONE_CYCLE;
-	S9xAPUExecute ();
 	while (CPU.Cycles >= CPU.NextEvent)
 		S9xDoHEventProcessing();
 
@@ -1417,8 +1416,6 @@ void S9xStartHDMA (void)
 			DMA[i].DoTransfer = FALSE;
 	}
 
-	S9xAPUExecute ();
-
 	CPU.InHDMA = FALSE;
 	CPU.InDMAorHDMA = CPU.InDMA;
 	CPU.HDMARanInDMA = CPU.InDMA ? PPU.HDMA : 0;
@@ -1743,9 +1740,7 @@ uint8 S9xDoHDMA (uint8 byte)
 		}
 	}
 
-	S9xAPUExecute ();
-
-	CPU.InHDMA = FALSE;
+        CPU.InHDMA = FALSE;
 	CPU.InDMAorHDMA = CPU.InDMA;
 	CPU.InWRAMDMAorHDMA = temp;
 	CPU.CurrentDMAorHDMAChannel = tmpch;
